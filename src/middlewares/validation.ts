73ff522346b0dfae879c0533da3bbdb9c6ec3f97
Youcef <youcef.zahariou@gmail.com>
2019-12-20 11:19:23 +0100

    adding tests with jest

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -3,7 +3,7 @@ import { signUpSchema, parseErrorSchema } from "../config/schemas";
 
 export function validateSignUpBody(req: Request, res: Response, next: NextFunction) {
   const { error } = signUpSchema.validate(req.body);
-  if (error) return res.status(422).json({ message: error.message });
+  if (error) return res.status(400).json({ message: error.message });
 
   next();
 }
