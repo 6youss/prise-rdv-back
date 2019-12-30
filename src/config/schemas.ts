374bda88760f569263f4353fe469e0e896e23b4c
Youcef <youcef.zahariou@gmail.com>
2019-12-30 14:45:10 +0100

    added joi custom error message validation

--- src/config/schemas.ts
+++ src/config/schemas.ts
@@ -1,5 +1,37 @@
 import Joi from "@hapi/joi";
 
+export const DoctorProfileSchema = Joi.object({
+  firstName: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required(),
+
+  lastName: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required(),
+
+  holidays: Joi.array()
+    .items(Joi.date())
+    .required()
+}).required();
+
+export const PatientProfileSchema = Joi.object({
+  firstName: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required(),
+
+  lastName: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required()
+}).required();
+
 export const signUpSchema = Joi.object({
   username: Joi.string()
     .alphanum()
@@ -13,19 +45,23 @@ export const signUpSchema = Joi.object({
     .valid(Joi.ref("password"))
     .required(),
 
-  userType: Joi.string().required(),
+  userType: Joi.string()
+    .valid(...["doctor", "patient"])
+    .required(),
 
-  profile: Joi.object({
-    firstName: Joi.string()
-      .alphanum()
-      .min(3)
-      .max(30)
-      .required(),
+  profile: Joi.object().required()
+}).required();
 
-    lastName: Joi.string()
-      .alphanum()
-      .min(3)
-      .max(30)
-      .required()
-  }).required()
+export const loginSchema = Joi.object({
+  username: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .not(Joi.ref("password"))
+    .messages({
+      "any.invalid": "username and password must be different"
+    })
+    .required(),
+
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
 }).required();
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
 import passport from "passport";
-import { validateSignUpBody } from "../middlewares/validation";
+import { validateSignUpBody, validateLoginBody } from "../middlewares/validation";
 
 const router = Router();
 
 router.get("/", passport.authenticate("jwt", { session: false }), UserController.getUser);
-router.post("/login", UserController.postLogin);
+router.post("/login", validateLoginBody, UserController.postLogin);
 router.post("/signup", validateSignUpBody, UserController.postSignup);
 router.post("/token", UserController.refreshToken);
 
