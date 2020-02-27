9d29e4d283733ae453742f8b26618791e0afee14
Youcef ZAHARIOU <youcef.zahariou@gmail.com>
2020-02-27 16:37:49 +0100

    editing the doctor's model

--- src/models/Doctor.ts
+++ src/models/Doctor.ts
@@ -1,21 +1,42 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, {Schema, Document} from 'mongoose';
+
+export interface DateRange {
+  from: Date;
+  to: Date;
+}
+export interface SessionDuration extends DateRange {
+  duration: string;
+}
+
+export interface WorkingHours extends DateRange {
+  start: string;
+  end: string;
+}
+
+export type ReservationType = 'counter' | 'time';
 
 export interface IDoctor extends Document {
   firstName: string;
   lastName: string;
   phone: string;
   address: string;
-  holidays: [Date];
+  unavailablities: Array<DateRange>;
+  workingHours: Array<WorkingHours>;
+  sessionDurations: Array<SessionDuration>;
+  reservationType: ReservationType;
 }
 
 const DoctorSchema = new Schema({
   firstName: String,
   lastName: String,
-  phone: { type: String, required: true, unique: true },
+  phone: String,
   address: String,
-  holidays: [Date]
+  unavailablities: [{from: Date, to: Date}],
+  workingHours: [{from: Date, to: Date, start: String, end: String}],
+  sessionDurations: [{from: Date, to: Date, duration: String}],
+  reservationType: {type: String, enum: ['counter', 'time'], default: 'time'},
 });
 
-const Doctor = mongoose.model<IDoctor>("Doctor", DoctorSchema, "doctor");
+const Doctor = mongoose.model<IDoctor>('Doctor', DoctorSchema, 'doctor');
 
 export default Doctor;
